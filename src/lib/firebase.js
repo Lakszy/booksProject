c8f742fda49d0a1439810e4e63c1c5a42a2c32db
Binova Gautam <binova1245@gmail.com>
2023-12-27 11:24:40 +0530

    firebase initialized

---
+++ src/lib/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDsVlng-JMTu3hIkea2-2jQa-6H1dEA0_4",
+    authDomain: "ccs-panel.firebaseapp.com",
+    databaseURL: "https://ccs-panel-default-rtdb.firebaseio.com",
+    projectId: "ccs-panel",
+    storageBucket: "ccs-panel.appspot.com",
+    messagingSenderId: "87670949063",
+    appId: "1:87670949063:web:d50590a72e7ae4945b2ae2",
+    measurementId: "G-XMHXVQFTWD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = app.auth();
+
+export default app;
+export {auth};
