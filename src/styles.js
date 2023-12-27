6f8d6aa530cdc1448bdefcd1ac697c952edb3f10
Lakshay Khattar <lakshaykhattar0208@gmail.com>
2023-12-27 16:35:45 +0530

    updatedStyle

---
+++ src/styles.js
@@ -0,0 +1,59 @@
+const loginStyles = {
+    loginBox: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      flexDirection: "row-reverse",
+      maxWidth: "1000px",
+      overflow: "hidden",
+      margin: "0 auto",
+      borderRadius: "12px",
+      marginTop: "100px",
+    },
+    loginForm: {
+      flex: "1 0 100%",
+      maxWidth: "480px",
+      width: "100%",
+      padding: "60px",
+      marginBottom: "80px",
+    },
+    header: {
+      fontSize: "24px",
+      marginBottom: "20px",
+    },
+    inputContainer: {
+      marginBottom: "15px",
+    },
+    label: {
+      marginRight: "10px",
+      fontSize: "16px",
+    },
+    input: {
+      padding: "10px",
+      fontSize: "16px",
+      width: "100%",
+      boxSizing: "border-box",
+      marginBottom: "15px",
+      border: "1px solid #555",
+      borderRadius: "5px",
+    },
+    error: {
+      color: "red",
+      marginTop: "10px",
+    },
+    buttonContainer: {},
+    button: {
+      padding: "10px",
+      fontSize: "16px",
+      backgroundColor: "#4CAF50",
+      color: "white",
+      border: "none",
+      borderRadius: "5px",
+      cursor: "pointer",
+      width: "100%",
+      boxSizing: "border-box",
+    },
+  };
+  
+  export default loginStyles;
+  
